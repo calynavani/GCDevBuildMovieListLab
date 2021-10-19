ed4610c748dd372d5976cf94bbf9a78ef55ea717
Calyn Greene <calyngreene@rocketmortgage.com>
2021-10-19 08:42:22 -0400

    second commit for Movie List Lab

--- GCDevBuildMovieListLab/Program.cs
+++ GCDevBuildMovieListLab/Program.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 namespace GCDevBuildMovieListLab
 {
     class Program
+    //How is a class different from an object? How are public methods different from private?
+    //classes can be thought of more as a blue print or insturctions of how to create an object. We must first create a class 
+    // in order to create an object An object is a product or instance of that class. Public methods are accessible from other classes
+    //while priavte methods are only accessible from  that related class and realted fields.
     {
         static void Main(string[] args)
         {
